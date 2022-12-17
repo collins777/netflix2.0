4d8c760064918463bf1ffd671654d49f2e486b08
Brandon Collins <38891846+collins777@users.noreply.github.com>
2022-12-17 13:46:01 -0500

    added banner component

--- src/components/HomeScreen/HomeScreen.jsx
+++ src/components/HomeScreen/HomeScreen.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import "./HomeScreen.css";
 import Nav from "../Nav/Nav";
+import Banner from "../Banner/Banner";
 
 const HomeScreen = () => {
   return (
     <div className="homeScreen">
-      {/* Navbar */}
       <Nav />
-
-      {/* Banner */}
+      <Banner />
       {/* Rows */}
     </div>
   );
